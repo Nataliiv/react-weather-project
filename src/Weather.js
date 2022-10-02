4545db01a6c9905dd9c8759ff181fa8cd858ce38
Nataliiv <nataliya1986kaduk@gmail.com>
2022-10-02 17:03:20 +0300

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form className="mb-4">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-              autoFocus="on"
-            />
+export default function Weather(props) {
+	const [weatherData, setWeatherData] = useState({ ready: false });
+	let [city, setCity] = useState(props.defaultCity);
+	function handleResponse(response) {
+		console.log(response.data);
+		setWeatherData ({
+			ready: true,
+			city: response.data.name,
+			temperature: response.data.main.temp,
+			humidity: response.data.main.humidity,
+			wind: response.data.wind.speed,
+			icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+			description: response.data.weather[0].description,
+			country: response.data.sys.country,
+		});
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		search();
+	}
+
+	function handleCityChange(event) {
+		setCity(event.target.value);
+	}
+
+	function search() {
+		const apiKey = "3743a596ca777c1b75d0b29a0dd4cdfd";
+		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		axios.get(apiUrl).then(handleResponse);
+	}
+
+	if (weatherData.ready) {
+		return (
+      <div className="Weather">
+        <form className="mb-4" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+								onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-info w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-info w-100"
-            />
+        </form>
+        <div className="weatherInfo">
+          <div className="row">
+            <div className="col-6">
+              <div className="d-flex">
+                <div>
+                  <img
+                    src={weatherData.icon}
+                    alt={weatherData.description}
+                  />
+                </div>
+                <div>
+                  <span className="temperature">{Math.round(weatherData.temperature)}</span>
+                  <span className="unit">°C | °F</span>
+                </div>
+              </div>
+              <div className="description">
+                <span className="me-3">Humidity: {weatherData.humidity}%</span>
+                <span>Wind: {weatherData.wind} km/h</span>
+              </div>
+            </div>
+            <div className="col-6 citySearch">
+              <h1>{weatherData.city}, {weatherData.country}</h1>
+              <ul>
+                <li>Saturday 18:00</li>
+                <li className="text-capitalize">{weatherData.description}</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </form>
-			<div className="weatherInfo">
-				<div className="row">
-					<div className="col-6">
-						<div className="d-flex">
-							<div>
-								<img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="" />
-							</div>
-							<div>
-								<span className="temperature">17</span>
-								<span className="unit">°C | °F</span> 
-							</div>					
-						</div>
-						<div className="description">
-							<span className="me-3">Humidity: 61%</span>
-							<span>Wind: 7 km/h</span>
-						</div>
-      		</div>
-					<div className="col-6 citySearch">
-						<h1>London, UK</h1>
-						<ul>
-							<li>Saturday 18:00</li>
-							<li>Few Clouds</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-    </div>
-  );
+      </div>
+    );
+	} else {
+		search();
+		return (
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="#8ef6e4"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+    );
+	}
+  
 }
