2e25e2619f1283428aa5419cfae575475e19ce0d
Nataliiv <nataliya1986kaduk@gmail.com>
2022-10-02 19:38:20 +0300

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="London" />
-      </div>      
+        <footer>
+          This project was coded by Nataliia Ivanchak and is {" "}
+          <a
+            href="https://github.com/Nataliiv/react-weather-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
