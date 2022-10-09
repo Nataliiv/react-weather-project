ba4684a04052c470d35f47bf3f5d967153733e3a
Nataliiv <nataliya1986kaduk@gmail.com>
2022-10-09 14:29:36 +0300

    Added forecast of 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,9 +17,15 @@ export default function WeatherForecast(props) {
 		return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">
-          <WeatherForecastDay data={forecast[0]} />
+				{forecast.map(function(day, index) {
+					if (index < 5) {
+						return <div className="col" key={index}>
+          <WeatherForecastDay data={day} />
         </div>
+					} else {
+						return null;
+					};
+				})}
       </div>
     </div>
   ); 
